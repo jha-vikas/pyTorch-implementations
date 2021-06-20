b2c859e50803850adae430bfb02bebf8d628f598
jha-vikas <vikasjhanitk@gmail.com>
2021-06-20 11:02:09 +0530

    Coding simple GAN

---
+++ simple_GAN.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from torch.utils.tensorboard import SummaryWriter
+
+# Discriminator
+class Discriminator
